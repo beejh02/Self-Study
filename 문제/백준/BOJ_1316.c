b43c053863234d6ac5d9783dd39a99cf9a5cda3b
SiRyung <joon0448@naver.com>
2024-07-02 16:40:12 +0900

    add problem

--- 문제/백준/BOJ_1316.c
+++ 문제/백준/BOJ_1316.c
@@ -2,40 +2,30 @@
 #include <string.h>
 
 int main(){
-    int n, size;
-    int cnt;
-    int r;
-    
+    int n, size, cnt, r;
     scanf("%d",&n);
-    
-    cnt = n;
+    cnt =n;
     
     for(int i=0; i<n; i++){
-        char a[101] = {0,};
+        char a[101]={0};
         scanf("%s",a);
         size = strlen(a);
-        
-        for(int j = 0; j < size; j++){
+        for(int j=0; j<size; j++){
             r = 1;
             
-            if(a[j] != a[j+1]){
-                for(int k = j; k < size; k++){
-                    if(a[j] == a[k+2]){
+            if(a[j]!=a[j+1]){
+                for(int k=j; k<size; k++){
+                    if(a[j]==a[k+2]){
                         cnt--;
                         r--;
                         break;
                     }
                 } 
+                
                 if(r==0) break;
             }
         }
-        printf("%d\n",cnt);
     }
+    printf("%d",cnt);
     return 0;
 }
-
-
-
-
-
-
