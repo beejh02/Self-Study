2933cd4c257e337960e35682243df21f2a420571
SiRyung <joon0448@naver.com>
2024-07-08 15:49:23 +0900

    add problem

--- 문제/test.py
+++ 문제/test.py
@@ -1,10 +1,25 @@
-n = int(input())
-temp = n
-i = 2
+from collections import deque
 
-while(i <= n):
-    if(temp % i == 0):
-        print(i)
-        temp //= i
-    else:
-        i += 1
+n, m = map(int,input().split())
+arr = []
+queue = deque()
+directions = [(-1,0), (0,1), (0,-1), (1,0)]
+cnt = 0
+visited = [[0 for i in range(m)] for i in range(n)]
+
+for i in range(n):
+    arr.append(list(map(int,input())))
+
+queue.append((0,0))
+while(queue):
+    cury, curx = queue.popleft()
+
+    for dy, dx in directions:
+        ny = dy + cury
+        nx = dx + curx
+        
+        if(0 <= ny < n and 0 <= nx < m and arr[ny][nx] == 1 and visited[ny][nx] == 0):
+            visited[ny][nx] = 1
+            queue.append([ny,nx])
+            cnt += 1
+print(cnt-1)
