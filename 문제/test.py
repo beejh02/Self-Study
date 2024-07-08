4d04b2843397e8dbc7d7977a2b505443c1e97ee7
SiRyung <joon0448@naver.com>
2024-07-08 23:35:20 +0900

    add problem

--- 문제/test.py
+++ 문제/test.py
@@ -1,13 +1,12 @@
-arr = []
-
-for i in range(3):
-    arr.append(int(input()))
-
-if(arr[0] == 60 and arr[0] == arr[1] and arr[0] == arr[2]):
-    print("Equilateral")
-elif(sum(arr) == 180 and arr[0] == arr[1] or arr[1] == arr[2] or arr[0] == arr[2]):
-    print("Isosceles")
-elif(sum(arr) == 180):
-    print("Scalene")
-else:
-    print("Error")
+while(1):
+    arr = list(map(int,input().split()))
+    if(sum(arr) != 180):
+        print("Invalid")
+    elif(arr[0] == 0 and arr[1] == 0 and arr[2] == 0):
+        break
+    elif(arr[0] == arr[1] == arr[2]):
+        print("Equilateral")
+    elif(arr[0] == arr[1] or arr[1] == arr[2] or arr[0] == arr[2]):
+        print("Isosceles")
+    else:
+        print("Scalene") 
