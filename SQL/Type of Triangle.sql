d00dd59156e4c0148fdfc4039106dcd10bf985c9
SiRyung <joon0448@naver.com>
2024-07-10 17:29:27 +0900

    add problem

---
+++ SQL/Type of Triangle.sql
@@ -0,0 +1,16 @@
+SELECT 
+
+    CASE
+        WHEN NOT (A + B > C AND A + C > B AND B + C > A)
+            THEN 'Not A Triangle'
+            
+        WHEN A = B AND B = C
+            THEN 'Equilateral'
+            
+        WHEN A = B OR B = C OR A = C
+            THEN 'Isosceles'
+        ELSE 'Scalene'
+        
+    END AS triangle_type
+    
+FROM TRIANGLES;
