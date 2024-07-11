8c173830d47dfe3902571bef5939d54d3d94c695
SiRyung <joon0448@naver.com>
2024-07-11 17:57:08 +0900

    add problem

--- 문제/test.py
+++ 문제/test.py
@@ -1,18 +1,33 @@
-import heapq
+import sys
 
+setting = sys.stdin.readline()
 n = int(input())
-
-heap = []
-result = []
+index = len(setting)
 
 for i in range(n):
-    value = int(input())
-    if(value != 0):
-        heapq.heappush(heap, (abs(value), value))
-    else:
-        if(heap):
-            result.append(heapq.heappop(heap)[1])
+    enter = list(map(str,sys.stdin.readline().split()))
+    if(enter[0] == 'L'):
+        if(index == 0):
+            continue
         else:
-            result.append(0)
+            index -= 1
+    elif(enter[0] == 'D'):
+        if(index == len(setting)):
+            continue
+        else:
+            index += 1
+    elif(enter[0] == 'B'):
+        if(index == 0):
+            continue
+        else:
+            setting = setting[:index-1] + setting[index:]
+            index -= 1
+    else:
+        if(index == 0):
+            setting = enter[1] + setting
+            index += 1
+        else:
+            setting = setting[:index] + enter[1] + setting[index:]
+            index += 1
 
-print(result)
+sys.stdout.write(setting.replace("\n", ""))
