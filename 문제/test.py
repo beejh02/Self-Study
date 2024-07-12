7110795c2c5839a5e9985c8114f9e9f1c62fe595
SiRyung <joon0448@naver.com>
2024-07-12 14:19:40 +0900

    add problem

--- 문제/test.py
+++ 문제/test.py
@@ -1,30 +1,6 @@
-N = int(input())
-
-tree = {}
-for i in range(N):
-    root, left, right = map(str,input().split())
-    tree[root] = (left, right)
-
-def preorder(n):
-    if(n != '.'):
-        print(n, end = '')
-        preorder(tree[n][0])
-        preorder(tree[n][1])
-
-def inorder(n):
-    if(n != '.'):
-        inorder(tree[n][0])
-        print(n, end = '')
-        inorder(tree[n][1])
-
-def postorder(n):
-    if(n != '.'):
-        postorder(tree[n][0])
-        postorder(tree[n][1])
-        print(n, end = '')
-
-preorder('A')
-print()
-inorder('A')
-print()
-postorder('A')
+T = int(input())
+for _ in range(T):
+    N, M = map(int,input().split())
+    for i in range(M):
+        a,b = map(int,input().split())
+    print(N-1)
