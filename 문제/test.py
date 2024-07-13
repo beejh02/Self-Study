1ff7e4ae7ffb0ca35716d21ed4a5598c420d2346
SiRyung <joon0448@naver.com>
2024-07-13 18:57:07 +0900

    add problem

--- 문제/test.py
+++ 문제/test.py
@@ -1,6 +1,7 @@
-T = int(input())
-for _ in range(T):
-    N, M = map(int,input().split())
-    for i in range(M):
-        a,b = map(int,input().split())
-    print(N-1)
+n = "932"
+
+a = int(n[0])
+b = len(n)
+
+print(a*b)
+
