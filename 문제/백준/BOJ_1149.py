a39afa8af5a0f7a097c99249778a088d3fecda8d
SiRyung <joon0448@naver.com>
2024-07-18 23:53:11 +0900

    add problem

--- 문제/백준/BOJ_1149.py
+++ 문제/백준/BOJ_1149.py
@@ -10,7 +10,6 @@ for i in range(1, len(arr)):
     arr[i][1] = min(arr[i-1][0],arr[i-1][2])+arr[i][1]
     arr[i][2] = min(arr[i-1][0],arr[i-1][1])+arr[i][2]
 
-
 a = arr[N-1][0]
 b = arr[N-1][1]
 c = arr[N-1][2]
