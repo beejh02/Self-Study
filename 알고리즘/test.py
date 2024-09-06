557e701c1441e0b06cd12dd42ddf6163cb49c63e
SiRyung <joon0448@naver.com>
2024-09-07 00:21:45 +0900

    add problem

--- 알고리즘/test.py
+++ 알고리즘/test.py
@@ -1,4 +1,17 @@
-'''컴파일용 파일입니다'''
-print(".  .   .")
-print("|  | _ | _. _ ._ _  _")
-print("|/\|(/.|(_.(_)[ | )(/.")
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+num = [*map(int, input().split())]
+
+prefix_sum = [0]
+temp = 0
+
+for i in num:
+    temp += i
+    prefix_sum.append(temp)
+
+for j in range(m):
+    a, b = map(int, input().split())
+    print(prefix_sum[b] - prefix_sum[a-1])
