d811705928d87810934edb47e20a43edfba03912
SiRyung <joon0448@naver.com>
2024-09-10 23:31:29 +0900

    add

--- 컴파일.py
+++ 컴파일.py
@@ -1,26 +1,8 @@
-import sys
+n = int(input())
 
-S = sys.stdin.readline().strip()
+arr = []
 
-psum = [[0 for _ in range(len(S))] for _ in range(26)]
+for i in range(n):
+    arr.append(input())
 
-for i in range(len(S)):
-    if(i > 0):
-        for j in range(26):
-            psum[j][i] = psum[j][i-1]
-    psum[ord(S[i]) - ord('a')][i] += 1
-
-Q = int(sys.stdin.readline())
-
-for _ in range(Q):
-    question = list(map(str, sys.stdin.readline().split()))
-    char = question[0]
-    question[1] = int(question[1])
-    question[2] = int(question[2])
-    
-    idx = ord(char) - ord('a')
-
-    if question[1] > 0:
-        print(psum[idx][question[2]] - psum[idx][question[1]-1])
-    else:
-        print(psum[idx][question[2]])
+print(set(arr))
