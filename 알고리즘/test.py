295076e4df62f2c285a11c3f75621fee352af21a
SiRyung <joon0448@naver.com>
2024-09-13 19:33:27 +0900

    unsolved problem 7576

--- 알고리즘/test.py
+++ 알고리즘/test.py
@@ -1,17 +1,33 @@
-import sys
-
-input = sys.stdin.readline
-n, m = map(int, input().split())
-
-num = [*map(int, input().split())]
-
-prefix_sum = [0]
-temp = 0
-
-for i in num:
-    temp += i
-    prefix_sum.append(temp)
-
-for j in range(m):
-    a, b = map(int, input().split())
-    print(prefix_sum[b] - prefix_sum[a-1])
+from collections import deque
+# import sys
+# input = sys.stdin.readline
+ 
+def solve(n, m, board):
+    queue = deque()
+    for i in range(m):
+        for j in range(n):
+            if board[i][j] == 1: queue.append([i, j, 0]) # x, y, 시간
+    # 동 남 서 북
+    dxs = (0, 1, 0, -1)
+    dys = (1, 0, -1, 0)
+ 
+    while queue:
+        x, y, t = queue.popleft()
+        T = t
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x+dx, y+dy
+            if nx<0 or nx>=m or ny<0 or ny>=n: continue
+            if board[nx][ny] == 0:
+                board[nx][ny] = 1
+                queue.append([nx, ny, t+1])
+    for i in range(m):
+        for j in range(n):
+            if board[i][j] == 0:
+                return -1
+    return T
+ 
+ 
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(m)]
+    print(solve(n, m, board))
--- 컴파일.py
+++ 컴파일.py
@@ -2,12 +2,12 @@ import sys
 from collections import deque
 
 N, M = map(int,input().split())
+cnt = 0
 
 arr = []
 queue = deque()
 
 directinos = [(-1,0), (0,1), (1,0), (0,-1)]
-
 
 ###
 # 입력한 배열 넣어주기
@@ -25,8 +25,10 @@ while(queue):
   cury, curx = queue.pop()
   for dy, dx in directinos:
     ny, nx = cury + dy, curx + dx
-    if(arr[ny][nx] == 0 and 0 < nx < M and 0 <= ny < N):
+    if(0 <= nx < N and 0 <= ny < M and arr[ny][nx] == 0):
       queue.append((ny,nx))
       arr[ny][nx] = 1
+    cnt += 1
 
 print(arr)
+print(cnt)
