635329ef91ab3b7641661ebf8cfd6c5d96499e21
이준혁 <91623756+beejh02@users.noreply.github.com>
2024-09-13 14:37:50 +0900

    Update 컴파일.py

--- 컴파일.py
+++ 컴파일.py
@@ -1,12 +1,32 @@
-n = int(input())
+import sys
+from collections import deque
 
-k = 0
+N, M = map(int,input().split())
 
-while(n > 0):
-    if(n%2 == 0):
-        n //= 2
-    else:
-        n -= 1
-        k += 1
+arr = []
+queue = deque()
 
-print(k)
+directinos = [(-1,0), (0,1), (1,0), (0,-1)]
+
+
+###
+# 입력한 배열 넣어주기
+for i in range(M):
+  arr.append(list(map(int,input().split())))
+
+###
+
+for i in range(M):
+  for j in range(N):
+    if(arr[i][j] == 1):
+      queue.append((i,j))
+
+while(queue):
+  cury, curx = queue.pop()
+  for dy, dx in directinos:
+    ny, nx = cury + dy, curx + dx
+    if(arr[ny][nx] == 0 and 0 < nx < M and 0 <= ny < N):
+      queue.append((ny,nx))
+      arr[ny][nx] = 1
+
+print(arr)
