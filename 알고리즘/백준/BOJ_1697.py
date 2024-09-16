ed7edf007a7334a19673b6e4d587b073f48260c7
SiRyung <joon@naver.com>
2024-09-16 16:48:31 +0900

    add problem

--- 알고리즘/백준/BOJ_1697.py
+++ 알고리즘/백준/BOJ_1697.py
@@ -1,9 +1,10 @@
+import sys
 from collections import deque
 
-N, M = map(int,input().split())
+N, M = map(int,sys.stdin.readline().split())
 
 queue = deque([N])
-arr = [0 for i in range(200001)]
+arr = [0 for i in range(100001)]
 arr[N] = 1
 cnt = 0
 
@@ -11,12 +12,10 @@ while(queue):
     for i in range(len(queue)):
         x = queue.popleft()
         if(x == M):
-            print(cnt)
+            sys.stdout.write(str(cnt))
             exit()
         for nx in (x-1, x+1, x*2):
-            if(0 < nx <= 200000 and arr[nx] == 0):
+            if(0 <= nx <= 100000 and arr[nx] == 0):
                 arr[nx] = 1
                 queue.append(nx)
     cnt += 1
-
-print(cnt)
