538e1af31d084c96f3114d8c5bf9e2c2e3243e77
SiRyung <joon0448@naver.com>
2024-11-15 01:03:21 +0900

    add problem

--- 컴파일.py
+++ 컴파일.py
@@ -1,2 +1,9 @@
-print("hello")
-print("백주용 안보여주기")
+arr = [1,10,5,8,7,6,4,3,2,9]
+
+for i in range(len(arr)):
+    find = i
+    for j in range(i+1, len(arr)):
+        if(arr[j] < arr[find]):
+            find = j
+    arr[i], arr[find] = arr[find], arr[i]
+    print(arr)
